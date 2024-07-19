f089fd64718c20a2597f8f88320a304dd1e91d96
vishnunaik-n-t <ntvishnunaik@gmail.com>
2024-07-19 16:06:32 +0530

    user Router add

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema=new mongoose.Schema({
     name:{
@@ -24,9 +25,9 @@ const userSchema=new mongoose.Schema({
         require:true,
     },
 
-    aadharCardNo:{
+    aadharCardNumber:{
         type:String,
-        require:ture,
+        require:true,
     },
 
     password:{
@@ -40,12 +41,42 @@ const userSchema=new mongoose.Schema({
         defoult:'voter',
     },
     isVoted:{
-        type:boolean,
+        type:Boolean,
         defoult:false
     }
 
 })
 
+
+userSchema.pre('save', async function(next){
+    const person = this;
+
+    // Hash the password only if it has been modified (or is new)
+    if(!person.isModified('password')) return next();
+    try{
+        // hash password generation
+        const salt = await bcrypt.genSalt(10);
+
+        // hash password
+        const hashedPassword = await bcrypt.hash(person.password, salt);
+        
+        // Override the plain password with the hashed one
+        person.password = hashedPassword;
+        next();
+    }catch(err){
+        return next(err);
+    }
+})
+
+userSchema.methods.comparePassword = async function(candidatePassword){
+    try{
+        // Use bcrypt to compare the provided password with the hashed password
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    }catch(err){
+        throw err;
+    }
+}
 
 const user=mongoose.model('user',userSchema);
 module.exports=user;
--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-const express=require('express');
-const app=express();
+const express = require('express')
+const app = express();
+const db = require('./db');
 require('dotenv').config();
 
 const bodyParser=require('body-parser');
 app.use(bodyParser.json())
-const PORT=process.env.PORT||300;
+const PORT=process.env.PORT||3000;
 
-app.get('/',function(req,res){
-    res.send("hello jarvis");
+const userRoutes = require('./routes/userRoutes');
+// const candidateRoutes = require('./routes/candidateRoutes');
+
+// Use the routers
+app.get('/',(req,res)=>{
+    res.send('well come to the world dude');
 })
-
+app.use('/user', userRoutes);
+// app.use('/candidate', candidateRoutes);
 app.listen(3000,()=>{
     console.log("listing on port no. 3000");
 });
